32739d60cc492baa6eda2700581094dd238821ee
RajuBehara05 <rajubehara373@gmail.com>
2022-02-28 13:32:47 +0530

    Add project files.

---
+++ ATM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    /// <summary>
+    /// ATM class to handle withdraw of notes
+    /// </summary>
+    public class ATM
+    {
+        private TwoThousandHandler twoThousandHandler = new TwoThousandHandler();
+        private FiveHundredHandler fiveHundredHandler = new FiveHundredHandler();
+
+        public ATM()
+        {
+            // Prepare the chain of Handlers
+            twoThousandHandler.nextHandler(fiveHundredHandler);
+        }
+        public void withdraw(long requestedAmount)
+        {
+            twoThousandHandler.dispatchRs(requestedAmount);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating new atm machine
+            ATM atm = new ATM();
+
+            //withdrawing amount
+            Console.WriteLine("\n Requested Amount 4500");
+            atm.withdraw(4500);
+            Console.WriteLine("\n Requested Amount 2000");
+            atm.withdraw(2000);
+            Console.WriteLine("\n Requested Amount 500");
+            atm.withdraw(500);
+
+            //holding console window
+            Console.Read();
+        }
+    }
+}
